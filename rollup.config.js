d5b0c412ea746c9d6175c0e728df739752c9589b
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-01-01 20:50:38 -0300

    v1.4.5

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { minify } from 'rollup-plugin-esbuild'
 import rollupPluginLicense from 'rollup-plugin-license';
 
 const bannerLicense = `/*!
-* @karasu-themes/blogger-utils v1.4.4
+* @karasu-themes/blogger-utils v1.4.5
 * https://github.com/Karasu-themes/bloggerSection
 *
 * Copyright © ${new Date().getFullYear()} MarceloTLD
